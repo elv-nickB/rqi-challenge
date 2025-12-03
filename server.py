0756e2aa4ac4098363ca5b51aef8dbda3bbe3708
Nicholas Buoncristiani <nick.buoncristiani@eluv.io>
2025-12-03 22:22:48 +0000

    added server starter file

---
+++ server.py
@@ -0,0 +1,15 @@
+"""
+Implement a web server that exposes a single endpoint for computing
+an eigenvalue/eigenvector pair using Rayleigh Quotient Iteration.
+"""
+
+def rayleigh_quotient_iteration():
+    # TODO: your code here
+    pass
+
+def main():
+    # Initialize and start your server here.
+    pass
+
+if __name__ == "__main__":
+    main()
